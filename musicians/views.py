e326c636ae8a8d97165f2a8ffab4c75f45721bfe
Antu Saha <antusaha970@gmail.com>
2024-05-30 21:37:16 +0600

    added all features

---
+++ musicians/views.py
@@ -0,0 +1,62 @@
+from django.shortcuts import render, redirect
+from .form import MusiciansForm, AlbumsForm
+from .models import Album, Musician
+# Create your views here.
+
+
+def add_musicians(request):
+    if request.method == 'POST':
+        form = MusiciansForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("view_musics")
+    else:
+        form = MusiciansForm()
+
+    return render(request, 'addMusicians.html', {'form': form})
+
+
+def add_album(request):
+    if request.method == 'POST':
+        form = AlbumsForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("view_musics")
+    else:
+        form = AlbumsForm()
+
+    return render(request, 'addAlbums.html', {'form': form})
+
+
+def view_musics(request):
+    data = Album.objects.all()
+    return render(request, 'viewMusics.html', {'data': data})
+
+
+def delete_album(request, id):
+    album = Album.objects.get(pk=id)
+    album.delete()
+    return redirect("view_musics")
+
+
+def edit_album(request, id):
+    album = Album.objects.get(pk=id)
+    form = AlbumsForm(instance=album)
+    if request.method == 'POST':
+        form = AlbumsForm(request.POST, instance=album)
+        if form.is_valid():
+            form.save()
+            return redirect("view_musics")
+    return render(request, 'addAlbums.html', {'form': form})
+
+
+def edit_musicians(request, id):
+    musician = Musician.objects.get(pk=id)
+    form = MusiciansForm(instance=musician)
+    if request.method == 'POST':
+        form = MusiciansForm(request.POST, instance=musician)
+        if form.is_valid():
+            form.save()
+            return redirect("view_musics")
+
+    return render(request, 'editMusicians.html', {'form': form})
